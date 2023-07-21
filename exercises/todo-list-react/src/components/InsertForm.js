eca6e835a7d42067deb80d3d61303830bee1c4d9
[SW5] 송서현 <seohyunsong03@gmail.com>
2023-07-21 13:27:26 +0900

    refactor: useCallback, error check 문 작성

--- exercises/todo-list-react/src/components/InsertForm.js
+++ exercises/todo-list-react/src/components/InsertForm.js
@@ -1,18 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 
 const InsertForm = ({ onInsert }) => {
 	const [inputValue, setInputValue] = useState("");
-	//   const handleChangeTodo = event => {
-	//     setInputValue(event.target.value);
-	//   };
-	const handleSubmit = (event) => {
-		event.preventDefault();
-		if (typeof onInsert === "function") {
-			// 존재하면서 타입이 함수여야 호출한다.
-			onInsert(inputValue);
-		}
-		setInputValue("");
-	};
+	// 컴포넌트가 재렌더링 될 때 불필요하게 함수가 다시 생성되는 것을 방지,
+	// deps로 설정한 값이 변경될 때만 수행
+	const handleSubmit = useCallback(
+		(event) => {
+			event.preventDefault();
+
+			// onInsert가 정상적인 함수인지 확인하여 에러 방지
+			// input값이 null 일떼 함수 실행할 필요없음
+			if (typeof onInsert === "function" && inputValue) {
+				onInsert(inputValue);
+			}
+			setInputValue("");
+		},
+		[onInsert, inputValue]
+	);
 	return (
 		<form onSubmit={handleSubmit}>
 			<input
