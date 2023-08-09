b813bde9c3e8b4d90ec5de5e84d4880f1f98f6b0
seohyun <amanda94@naver.com>
2023-08-09 19:44:08 +0900

    add: useReducer로 요청 상태 관리하기

--- exercises/react-api/src/components/Users.js
+++ exercises/react-api/src/components/Users.js
@@ -1,35 +1,60 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useReducer } from "react";
 import axios from "axios";
 
+/* 리듀서 */
+// useState의 setState 함수를 여러 번 사용하지 않아도 된다.
+// 리듀서 로직을 분리했으므로 다른곳에 쉽게 재사용 할 수 있다.
+function reducer(state, action) {
+	switch (action.type) {
+		case "LOADING":
+			return {
+				loading: true,
+				data: null,
+				error: null,
+			};
+		case "SUCCESS":
+			return {
+				loading: false,
+				data: action.data,
+				error: null,
+			};
+		case "ERROR":
+			return {
+				loading: false,
+				data: null,
+				error: action.error,
+			};
+		default:
+			throw new Error(`Unhandled action type: ${action.type}`);
+	}
+}
+
 export default function Users() {
-	/* useState : 요청 상태 관리 */
-	const [users, setUsers] = useState(null); // 요청의 결과
-	const [loading, setLoading] = useState(false); // 로딩 상태
-	const [error, setError] = useState(null); // 에러
+	/* useReducer :  상태 업데이트 로직 분리 */
+	const [state, dispatch] = useReducer(reducer, {
+		loading: false,
+		data: null,
+		error: null,
+	});
 
-	// 버튼 눌러서 API 재요청하는 기능 구현하기 위해 fetchUsers 함수 useEffect 바깥으로 꺼내주었습니다.
 	const fetchUsers = async () => {
+		dispatch({ type: "LOADING" });
 		try {
-			// 요청이 시작할 때는 error와 users 초기화
-			setError(null);
-			setUsers(null);
-
-			// loading 상태를 true로 바꿉니다.
-			setLoading(true);
-
 			const response = await axios.get(
 				"https://jsonplaceholder.typicode.com/users"
 			);
-			setUsers(response.data);
+			dispatch({ type: "SUCCESS", data: response.data });
 		} catch (e) {
-			setError(e);
+			dispatch({ type: "ERROR", error: e });
 		}
-		setLoading(false);
 	};
+
 	/* useEffect(()=> {}, []): 컴포넌트가 마운트 됐을때 실행(첫 렌더링) */
 	useEffect(() => {
 		fetchUsers();
 	}, []);
+
+	const { loading, data: users, error } = state;
 
 	if (loading) return <div>로딩중 ..</div>;
 	if (error) return <div>에러가 발생했습니다.</div>;
