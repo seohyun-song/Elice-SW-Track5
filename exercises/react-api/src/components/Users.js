8de8960436968616323afc0870bb484999507123
seohyun <amanda94@naver.com>
2023-08-09 19:30:01 +0900

    add: 버튼 눌러서API 재요청하기

--- exercises/react-api/src/components/Users.js
+++ exercises/react-api/src/components/Users.js
@@ -7,28 +7,27 @@ export default function Users() {
 	const [loading, setLoading] = useState(false); // 로딩 상태
 	const [error, setError] = useState(null); // 에러
 
+	// 버튼 눌러서 API 재요청하는 기능 구현하기 위해 fetchUsers 함수 useEffect 바깥으로 꺼내주었습니다.
+	const fetchUsers = async () => {
+		try {
+			// 요청이 시작할 때는 error와 users 초기화
+			setError(null);
+			setUsers(null);
+
+			// loading 상태를 true로 바꿉니다.
+			setLoading(true);
+
+			const response = await axios.get(
+				"https://jsonplaceholder.typicode.com/users"
+			);
+			setUsers(response.data);
+		} catch (e) {
+			setError(e);
+		}
+		setLoading(false);
+	};
 	/* useEffect(()=> {}, []): 컴포넌트가 마운트 됐을때 실행(첫 렌더링) */
-	// useEffect에 첫 번째 파라미터로 등록하는 함수에는 async를 사용할 수 없기 때문에
-	// async를 사용하는 새로운 함수를 선언해주어야 합니다.
 	useEffect(() => {
-		const fetchUsers = async () => {
-			try {
-				// 요청이 시작할 때는 error와 users 초기화
-				setError(null);
-				setUsers(null);
-
-				// loading 상태를 true로 바꿉니다.
-				setLoading(true);
-
-				const response = await axios.get(
-					"https://jsonplaceholder.typicode.com/users"
-				);
-				setUsers(response.data);
-			} catch (e) {
-				setError(e);
-			}
-			setLoading(false);
-		};
 		fetchUsers();
 	}, []);
 
@@ -36,12 +35,15 @@ export default function Users() {
 	if (error) return <div>에러가 발생했습니다.</div>;
 	if (!users) return null;
 	return (
-		<ul>
-			{users.map((user) => (
-				<li key={user.id}>
-					{user.username} ({user.name})
-				</li>
-			))}
-		</ul>
+		<>
+			<ul>
+				{users.map((user) => (
+					<li key={user.id}>
+						{user.username} ({user.name})
+					</li>
+				))}
+			</ul>
+			<button onClick={fetchUsers}>다시 불러오기</button>
+		</>
 	);
 }
